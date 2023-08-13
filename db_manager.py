ea2eb35539d22ca9b40ab4a42bb9066669e0c9b9
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-08-13 13:22:39 +0300

    Added descriptions to the class methods

--- db_manager.py
+++ db_manager.py
@@ -1,73 +1,103 @@
-from config import config
+from typing import Tuple, List, Any
 import psycopg2
 
 
-class DBManager():
+class DBManager:
 	"""Класс для работы с базой данных вакансий"""
+	def __init__(self, db_name: str, params: dict) -> None:
+		"""
+		Инициализатор параметров для подключения к БД
+		:param db_name: Имя БД
+		:param params: Параметры для подключения к БД
+		"""
+		self.db_name = db_name
+		self.params = params
 
-
-	def __init__(self, db_name: str):
-		"""Set the database name"""
-		# self.db_name = db_name
-		# self.conn = psycopg2.connect(dbname='postgres', **self.params)
-		# self.conn.autocommit = True
-		# self.cur = self.conn.cursor()
-		# try:
-		# 	with self.cur:
-		# 		self.cur.execute(f'DROP DATABASE IF EXISTS {self.db_name}')
-		# 		self.cur.execute(f'CREATE DATABASE {self.db_name}')
-		# 		self.cur.execute(f"""CREATE TABLE employers
-		# 		(
-		# 		emp_id int PRIMARY KEY,
-		# 		emp_name varchar(100) NOT NULL,
-		# 		emp_url varchar NOT NULL
-		# 		);""")
-		# 		self.cur.execute(f"""CREATE TABLE vacancies
-		# 		(
-		# 		vac_id int PRIMARY KEY,
-		# 		vac_name varchar(100) NOT NULL,
-		# 		city varchar(30),
-		# 		salary int NOT NULL,
-		# 		vac_url varchar NOT NULL,
-		# 		published_date date,
-		# 		emp_id int
-		# 		);
-		#
-		# 		ALTER TABLE vacancies ADD CONSTRAINT fk_vacancies_employers
-		# 		FOREIGN KEY (emp_id) REFERENCES employers(emp_id);
-		# 		""")
-		# finally:
-		# 	self.conn.close()
-
-
-
-	# def __enter__(self):
-	# 	pass
-	#
-	# def __exit__(self):
-	# 	pass
-
-	def save_data_to_database(self, employers_data: dict, vacancies_data: dict):
-		pass
-
-	def get_companies_and_vacancies_count(self):
+	def get_companies_and_vacancies_count(self) -> Tuple[List[str], List[tuple[Any, ...]]]:
 		"""Получает список всех компаний и количество вакансий у каждой компании"""
-		pass
+		conn = psycopg2.connect(dbname=self.db_name, **self.params)
+		conn.autocommit = True
 
-	def get_all_vacancies(self):
+		with conn.cursor() as cur:
+			cur.execute(
+				"""
+				SELECT employers.emp_name, COUNT(*) FROM vacancies
+				JOIN employers ON employers.emp_id=vacancies.emp_id
+				GROUP BY employers.emp_name
+				"""
+			)
+			data = cur.fetchall()
+		conn.close()
+		return ['Company name', 'Numbers of vacancies'], data
+
+	def get_all_vacancies(self) -> Tuple[List[str], List[tuple[Any, ...]]]:
 		"""
 		Получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию.
 		"""
-		pass
+		conn = psycopg2.connect(dbname=self.db_name, **self.params)
+		conn.autocommit = True
 
-	def get_avg_salary(self):
+		with conn.cursor() as cur:
+			cur.execute(
+				"""
+				SELECT e.emp_name, v.vac_name, v.salary, v.vac_url FROM vacancies AS v
+				JOIN employers AS e ON  e.emp_id=v.emp_id
+				"""
+			)
+			data = cur.fetchall()
+		conn.close()
+		return ['Company name', 'Vacancy name', 'Salary', 'Vacancy link'], data
+
+	def get_avg_salary(self) -> Tuple[List[str], List[tuple[Any, ...]]]:
 		"""Получает среднюю зарплату по вакансиям."""
-		pass
+		conn = psycopg2.connect(dbname=self.db_name, **self.params)
+		conn.autocommit = True
 
-	def get_vacancies_with_higher_salary(self):
+		with conn.cursor() as cur:
+			cur.execute(
+				"""
+				SELECT AVG(salary) FROM vacancies
+				WHERE salary > 0
+				"""
+			)
+			data = cur.fetchall()[0][0]
+		conn.close()
+		if not data:
+			data = 0
+		else:
+			data = float(data)
+		return ['Description', 'Quantity'], [('Average salary', data)]
+
+	def get_vacancies_with_higher_salary(self) -> Tuple[List[str], List[tuple[Any, ...]]]:
 		"""Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
-		pass
+		conn = psycopg2.connect(dbname=self.db_name, **self.params)
+		conn.autocommit = True
 
-	def get_vacancies_with_keyword(self, keyword: str):
+		with conn.cursor() as cur:
+			cur.execute(
+				"""
+				SELECT e.emp_name, v.vac_name, v.salary, v.vac_url FROM vacancies AS v
+				JOIN employers AS e ON  e.emp_id=v.emp_id
+				WHERE v.salary > (SELECT AVG(salary) FROM vacancies WHERE salary > 0)
+				"""
+			)
+			data = cur.fetchall()
+		conn.close()
+		return ['Company name', 'Vacancy name', 'Salary', 'Vacancy link'], data
+
+	def get_vacancies_with_keyword(self, keyword: str) -> Tuple[List[str], List[tuple[Any, ...]]]:
 		"""Получает список всех вакансий, в названии которых содержатся переданные в метод слова"""
-		pass
+		conn = psycopg2.connect(dbname=self.db_name, **self.params)
+		conn.autocommit = True
+
+		with conn.cursor() as cur:
+			cur.execute(
+				f"""
+				SELECT * FROM vacancies as v
+				WHERE v.vac_name LIKE '%{keyword.lower()}%' or v.vac_name LIKE '%{keyword.upper()}%' or
+				v.vac_name LIKE '%{keyword.capitalize()}%'
+				"""
+			)
+			data = cur.fetchall()
+		conn.close()
+		return ['VacancyID', 'Vacancy name', 'City', 'Salary', 'Vacancy link','Published date', 'EmployerID'], data
